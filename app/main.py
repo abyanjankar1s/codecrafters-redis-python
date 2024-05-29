6071e639a5e84f5c048d66debd12e40867aaa61a
Aayush <aayush.byanjankar28@gmail.com>
2024-05-29 09:22:24 -0500

    ping

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ def main():
     pong = "+PONG\r\n" 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    
+    #ok
     with conn:
         conn.recv(1024)
-        conn.send(pong.encode)
+        conn.send(pong.encode())
 
 
 if __name__ == "__main__":
