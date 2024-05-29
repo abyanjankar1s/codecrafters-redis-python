25d498ffbd6de51421454b40b80a318dcdaeefcb
Aayush <aayush.byanjankar28@gmail.com>
2024-05-29 09:40:48 -0500

    once more

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 #Uncomment this to pass the first stage
 import socket
+import re
 import threading
 
 
-def ping_client(client_socket):
-    client_socket.send(b"+PONG\r\n")
+
 
 def handle_connection(conn, addr):
     while True:
@@ -21,16 +21,14 @@ def handle_connection(conn, addr):
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_conn, addr = server_socket.accept() # wait for client
     while True:
-        if client_conn:
-            data = client_conn.recv(1024)
-            if not data:
-                break
-            client_conn.sendall(b"+PONG\r\n")
-        client_conn, addr = server_socket.accept() 
-        print(f"Received connection {client_conn}")
-        threading.Thread(target=handle_connection, args=[client_conn, addr])
+        client_socket, client_address = server_socket.accept()
+        print(f"Received a connection from {client_address}")
+        handle_connection(client_socket, client_address)
+        threading.Thread(
+            target=handle_connection, args=[client_socket, client_address]
+        ).start()
+        # handle_connection(client_socket, client_address)
 
 
 if __name__ == "__main__":
