004f088e3eb35427cf3fb4b07c973c3f08b3d4bb
Aayush <aayush.byanjankar28@gmail.com>
2024-05-29 09:37:56 -0500

    concurnt client

--- app/main.py
+++ app/main.py
@@ -1,23 +1,36 @@
 #Uncomment this to pass the first stage
 import socket
+import threading
+
+
+def ping_client(client_socket):
+    client_socket.send(b"+PONG\r\n")
+
+def handle_connection(conn, addr):
+    while True:
+        request: bytes = conn.recv(1024)
+        if not request:
+            break
+        data: str = request.decode()
+        if "ping" in data.lower():
+            response = "+PONG\r\n"
+            conn.send(response.encode())
+    conn.close()
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    pong = "+PONG\r\n" 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    #ok
-    with conn:
-        while True:
-            data = conn.recv(1024)
+    client_conn, addr = server_socket.accept() # wait for client
+    while True:
+        if client_conn:
+            data = client_conn.recv(1024)
             if not data:
                 break
-            print(f"{data=}")
-            conn.sendall(b"+PONG\r\n")
+            client_conn.sendall(b"+PONG\r\n")
+        client_conn, addr = server_socket.accept() 
+        print(f"Received connection {client_conn}")
+        threading.Thread(target=handle_connection, args=[client_conn, addr])
 
 
 if __name__ == "__main__":
