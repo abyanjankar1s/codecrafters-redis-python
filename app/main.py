19fbf36b76ccefc67629540c07959d4564bd9d7f
Aayush <aayush.byanjankar28@gmail.com>
2024-05-29 09:07:42 -0500

    p

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
-    client, addr = server_socket.accept() 
-    client.send(b"+PONG\r\n")
+    (conn, address) = server_socket.accept() 
+    while True:
+        with conn:
+            conn.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
