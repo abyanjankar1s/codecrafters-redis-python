b55a5b7523e7df1e4b783733aad67ef505aae743
Aayush <aayush.byanjankar28@gmail.com>
2024-05-29 09:42:34 -0500

    final

--- app/main.py
+++ app/main.py
@@ -14,13 +14,14 @@ def handle_connection(conn, addr):
         data: str = request.decode()
         if "ping" in data.lower():
             response = "+PONG\r\n"
+            print(response)
             conn.send(response.encode())
     conn.close()
 
 
 def main():
     print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6379))
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Received a connection from {client_address}")
